63b2af44985375a888ec67a632806781c5614bfe
henrispriet <henri.spriet@gmail.com>
2024-12-31 17:07:42 +0100

    comment out day25 impl

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,7 @@
 use aoc_runner_derive::aoc_lib;
 
-pub mod day25;
+// TODO: since cargo-aoc by default runs the latest implemented day,
+// I'll comment this out for now and make the other days in order first.
+// pub mod day25;
 
 aoc_lib!{ year = 2024 }
