afc3c80e8bd1bdcdb4a8b334c53229483bbd51b5
henrispriet <henri.spriet@gmail.com>
2024-12-31 16:59:17 +0100

    refactor to use cargo-aoc

--- src/day25.rs
+++ src/day25.rs
@@ -1,3 +1,5 @@
+use aoc_runner_derive::{aoc, aoc_generator};
+
 #[derive(Debug, PartialEq, Eq, Clone)]
 struct InputData {
     keys: Vec<Key>,
@@ -12,6 +14,7 @@ struct Key([u8; PINS]);
 #[derive(Debug, PartialEq, Eq, Clone, Copy)]
 struct Lock([u8; PINS]);
 
+#[aoc_generator(day25)]
 fn parse_it(input: &str) -> InputData {
     let (locks, keys) = input.split("\n\n").partition::<Vec<_>, _>(|x| {
         x.lines()
@@ -48,12 +51,13 @@ fn count_pin_lens<'a>(mut lines: impl Iterator<Item = &'a str>) -> [u8; PINS] {
     })
 }
 
-fn solve_it(InputData { keys, locks }: InputData) -> u64 {
+#[aoc(day25, part1)]
+fn solve_it(InputData { keys, locks }: &InputData) -> u64 {
     // naive: cartesian product
     let mut fits = 0;
 
-    for Key(k) in &keys {
-        for Lock(l) in &locks {
+    for Key(k) in keys {
+        for Lock(l) in locks {
             if std::iter::zip(k, l).all(|(k, l)| k + l < HEIGHT) {
                 fits += 1;
             }
@@ -61,88 +65,4 @@ fn solve_it(InputData { keys, locks }: InputData) -> u64 {
     }
 
     fits
-}
-
-fn do_it(input: &str) -> u64 {
-    solve_it(parse_it(input))
-}
-
-fn main() {
-    let input = include_str!("./puzzle_input.txt");
-    let result = do_it(input);
-    println!("{result}")
-}
-
-#[cfg(test)]
-mod test {
-    use super::*;
-
-    #[test]
-    fn parse_small() {
-        let result = parse_it(INPUT_SMALL);
-        assert_eq!(
-            result,
-            InputData {
-                keys: vec![
-                    Key([5, 0, 2, 1, 3]),
-                    Key([4, 3, 4, 0, 2]),
-                    Key([3, 0, 2, 0, 1])
-                ],
-                locks: vec![Lock([0, 5, 3, 4, 3]), Lock([1, 2, 0, 5, 3])]
-            }
-        );
-    }
-
-    #[test]
-    fn small() {
-        let result = do_it(INPUT_SMALL);
-        assert_eq!(result, 3);
-    }
-
-    #[test]
-    fn part1() {
-        let input = include_str!("puzzle_input.txt");
-        let result = do_it(input);
-        assert_eq!(result, 3483);
-    }
-
-    const INPUT_SMALL: &'static str = "#####
-.####
-.####
-.####
-.#.#.
-.#...
-.....
-
-#####
-##.##
-.#.##
-...##
-...#.
-...#.
-.....
-
-.....
-#....
-#....
-#...#
-#.#.#
-#.###
-#####
-
-.....
-.....
-#.#..
-###..
-###.#
-###.#
-#####
-
-.....
-.....
-.....
-#....
-#.#..
-#.#.#
-#####";
 }
---
+++ src/lib.rs
@@ -0,0 +1,5 @@
+use aoc_runner_derive::aoc_lib;
+
+pub mod day25;
+
+aoc_lib!{ year = 2024 }
