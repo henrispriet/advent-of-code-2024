a2d060b6dc547c772c886d8222dfa7b621f13a98
henrispriet <henri.spriet@gmail.com>
2025-01-01 13:32:27 +0100

    zero pad day numbers

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 use aoc_runner_derive::aoc_lib;
 
-pub mod day1;
-pub mod day2;
+pub mod day01;
+pub mod day02;
 // TODO: since cargo-aoc by default runs the latest implemented day,
 // I'll comment this out for now and make the other days in order first.
 // pub mod day25;
